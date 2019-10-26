31105b15bcc412dbac8d3deef969b07e775e74fa
annnta <phongpvt99@gmail.com>
2019-10-26 12:49:52 -0600

    minor changes to Employee

--- ObjectClass/Employee.py
+++ ObjectClass/Employee.py
@@ -3,7 +3,7 @@ import collections
 import PriorityQueue
 
 class Employee(object):
-	def __init__(self, firstName: str, lastName: str, department: [], skillSet: [], activeJobs: [], capacity: int):
+	def __init__(self, lastName: str, firstName: str, department: [], skillSet: [], activeJobs: [], capacity: int):
 		self.firstName = firstName
 		self.lastName = lastName
 		self.department = department
