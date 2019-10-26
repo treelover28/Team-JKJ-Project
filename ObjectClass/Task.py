8052209587bfbc16413f96c116a72057b7d311c0
annnta <phongpvt99@gmail.com>
2019-10-25 21:37:15 -0600

    Algorithm to get the assignment list

--- ObjectClass/Task.py
+++ ObjectClass/Task.py
@@ -1,15 +1,15 @@
 class Task:
     'Common base class for all task'
-    def __init__(self, name, department, skillSet, difficulty, length, activeEmployees, description, priority, assigned):
+    def __init__(self, name, department, skillSet, difficulty, length, activeEmployees, description, priority, assignedNeeded):
         self.name = name
-        self.department =department
+        self.department = department
         self.skillSet = skillSet
         self.difficulty = difficulty
         self.length = length
         self.activeEmployees = activeEmployees
         self.description = description
         self.priority = priority
-        self.assigned = assigned
+        self.assignedNeeded = assignedNeeded
 
     def __str__ (self):
         print ("Task: \n   Name:            ", self.name,
