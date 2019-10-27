a597465dac74360e8e98bdce7f122ab50082d56c
annnta <phongpvt99@gmail.com>
2019-10-27 11:16:35 -0600

    changes to folder

---
+++ Employee.py
@@ -0,0 +1,16 @@
+
+import collections
+import PriorityQueue
+
+class Employee(object):
+	def __init__(self, lastName: str, firstName: str, department: [], skillSet: [], activeJobs: [], capacity: int):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.department = department
+		self.skillSet = skillSet
+		self.activeJobs = PriorityQueue(activeJobs)
+		self.capacity = capacity
+
+if __name__ == "__main__":
+	employee = Employee("Alo", "Oke", ["IT", "IT2"], ["123", "456"], [1, 2, 3, 4], 100)
+	print(employee)
